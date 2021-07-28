b10c496844e6b439a6861617112932b4ef4fc344
ParkInTae <as3689@naver.com>
2021-07-28 22:58:14 +0900

    material-ui를 이용한 UI구성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
 
+{/** material를 이용한 css style 설정 */}
 
+const styles = theme => ({
+  root : {
+    width: '100%', marginTop: theme.spacing.unit * 3, overflowX:"auto"
+   },
+    table : {
+      minWidth: 768
+    } 
+})
 const customers = [
   {
   'id': 1,
@@ -33,23 +49,31 @@ const customers = [
 
 class App extends Component {
   render() {
+    const { classes } = this.props; {/** 위에 정의한 styles를 적용하기 위한 classes변수 선언*/}
     return(
-      <div>
-    { customers.map(t => {
-      return (
-        <Customer
-        key={t.id}
-        id = {t.id}
-        image = {t.image}
-        name = {t.name}
-        birthday={t.birthday}
-        gender={t.gender}
-        job={t.job}
-      />);
-    })
-    }
-    </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          { customers.map(t => {
+           return (<Customer  key={t.id}  id = {t.id}  image = {t.image}  name = {t.name}
+                    birthday={t.birthday}  gender={t.gender}  job={t.job} />);
+    }) }
+          </TableBody>
+        </Table>
+    
+    </Paper>
     )    
 }}
 
-export default App;
+export default withStyles (styles) (App);
+{/** withStyles를 적용하기 위해 내보내기 설정 */}
